4070a9e1519b450cd8bba2bb458ac4c04bfda363
SprinklerAi <eminlia.tilla@gmail.com>
2024-03-18 22:25:06 +0200

    Final version with corrected ids

--- app/src/main/java/atilla/studentregister/User.java
+++ app/src/main/java/atilla/studentregister/User.java
@@ -17,32 +17,19 @@ public class User {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getDegreeProgram() {
         return degreeProgram;
-    }
-
-    public void setDegreeProgram(String degreeProgram) {
-        this.degreeProgram = degreeProgram;
     }
 
 }
